7bccfd69eace33917c974650685eda493f74ce42
gguser0207 <freedoms777@naver.com>
2021-12-10 18:50:36 +0900

    수정 완료

--- src/test/java/dev/mvc/team6/Team6V2sbm3cApplicationTests.java
+++ src/test/java/dev/mvc/team6/Team6V2sbm3cApplicationTests.java
@@ -6,17 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ResortV1sbm3cApplicationTests {
-  @Autowired
-  private SqlSessionTemplate sqlSession;
-  
-  @Test
-  public void contextLoads() {
+class Team6V2sbm3cApplicationTests {
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+    
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testSqlSession() throws Exception{
+      System.out.println(sqlSession.toString());
+    }
+    
   }
 
-  @Test
-  public void testSqlSession() throws Exception{
-    System.out.println(sqlSession.toString());
-  }
-  
-}
